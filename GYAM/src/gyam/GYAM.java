72c6e8716c3e6b678ead293f18c2799fdb348663
GabrielaIsabel <60935537+GabrielaIsabel@users.noreply.github.com>
2020-06-22 13:25:25 -0500

    Se agrego GYAM

---
+++ GYAM/src/gyam/GYAM.java
@@ -0,0 +1,17 @@
+
+package gyam;
+
+/**
+ *
+ * @author DELL
+ */
+public class GYAM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- repogit/src/repogit/Repogit.java
+++
@@ -1,7 +0,0 @@
-package repogit;
-public class Repogit {
-    public static void main(String[] args) {
-        System.out.println("Hola");
-        System.out.println("hola gabyyyy");
-    }   
-}
